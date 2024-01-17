b32199330321e50e9c37ad6a3dca08e8a2f64444
05562849259 <kaiomm2000@gmail.com>
2024-01-17 20:43:42 -0300

    mudanca na regra de perfil/ obterUsuarioLogado add

--- lib/data/model/usuario.dart
+++ lib/data/model/usuario.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'package:comanda_full/main.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Usuario {
   final int id;
   final String nome;
   final String login;
   final String cpf;
-  final int perfil;
+  final List<int> perfil;
 
   Usuario(
       {required this.id,
@@ -21,14 +22,36 @@ class Usuario {
         id: map['id'],
         nome: map['nome'],
         login: map['login'],
-        cpf: map['cpf'],
-        perfil: map['perfil']);
+        cpf: map['cpf'] ?? '000000000-00',
+        perfil: (map['listaPerfil'] as List)
+            .map((item) => item['id'] as int)
+            .toList());
   }
-  static Future<List<Usuario>> fetchUsuarios() async {
-    final response = await http.get(Uri.parse('${url}/usuario'), headers: {
-      'Authorization':
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJrYWlvcHJvamVjdHMtand0Iiwic3ViIjoiQnJ1bm9DYXN0cm8iLCJncm91cHMiOlsiQWRtaW4iXSwiZXhwIjoxNzU3NDc3OTIxLCJpYXQiOjE2OTY5OTc5MjEsImp0aSI6IjNhZWIyZjJkLTA0OWYtNGE2OC1iYWEzLTMxZjkwM2E0YTA1ZiJ9.BJOn0lOYsoaCCZyNYkba8hDJ3-Je3H6X-RQT0EnH_cnNAu_Y0yPKrfMUj49CgDc2pMI9OnVrkJvjbaPKxGD2O5PlOjf9-VOAW4YdAdtSL-LjwKbtEDf83F9j-BW_-y898kQpmjpzCGNQtDQCHYVfT5VOZBvgmYSnF0kMJzIEm_7uirUlQCc6ekumSeNwdS4we019582ZZ2KBOc1ZDIXL-aC-3HvqbolrNjh0d0CaLG8w61kSBxOfUSHtyBtdADDQvhCK9vt_mI8ccigyv77yLtgdgM_mhQucwJFgVh4LNDDHjJWG6q7s10yEtRYbDoo2VauSxo60rpQoI1lHgpzjKQ'
+
+  static Future<Usuario> obterDadosUsuario() async {
+    final prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token') ?? '';
+
+    var baseUrl = Uri.parse('$url/usuariologado');
+
+    var resposta = await http.get(baseUrl, headers: {
+      'Authorization': 'Bearer $token',
     });
+
+    if (resposta.statusCode == 200) {
+      Map<String, dynamic> dados = json.decode(resposta.body);
+      return Usuario.fromMap(dados);
+    } else {
+      throw Exception('Falha ao carregar dados do usuário');
+    }
+  }
+
+  static Future<List<Usuario>> fetchUsuarios() async {
+    final prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token') ?? '';
+
+    final response = await http.get(Uri.parse('${url}/usuario'),
+        headers: {'Authorization': 'Bearer $token'});
     if (response.statusCode == 200) {
       print('response ok');
       final List<dynamic> jsonData = json.decode(response.body);
