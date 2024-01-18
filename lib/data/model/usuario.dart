bb1fb86ba015f2d83d3908d14b7b44cc733ec5a7
05562849259 <kaiomm2000@gmail.com>
2024-01-18 19:10:01 -0300

    inserir funcionario, e alterar criado

--- lib/data/model/usuario.dart
+++ lib/data/model/usuario.dart
@@ -4,23 +4,26 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Usuario {
-  final int id;
+  final int? id;
+  final bool? ativo;
   final String nome;
   final String login;
   final String cpf;
-  final List<int> perfil;
+  final List<int>? perfil;
 
   Usuario(
-      {required this.id,
+      {this.id,
       required this.nome,
+      this.ativo,
       required this.login,
       required this.cpf,
-      required this.perfil});
+      this.perfil});
 
   factory Usuario.fromMap(Map<String, dynamic> map) {
     return Usuario(
         id: map['id'],
         nome: map['nome'],
+        ativo: map['ativo'],
         login: map['login'],
         cpf: map['cpf'] ?? '000000000-00',
         perfil: (map['listaPerfil'] as List)
@@ -50,7 +53,7 @@ class Usuario {
     final prefs = await SharedPreferences.getInstance();
     String token = prefs.getString('token') ?? '';
 
-    final response = await http.get(Uri.parse('${url}/usuario'),
+    final response = await http.get(Uri.parse('${url}/gerente/funcionarios'),
         headers: {'Authorization': 'Bearer $token'});
     if (response.statusCode == 200) {
       print('response ok');
@@ -59,6 +62,67 @@ class Usuario {
     } else {
       print('falha ao carregar os Usuarios');
       throw Exception('Falha ao carregar os Usuarios');
+    }
+  }
+
+  static Future<void> inserirUsuario(
+      Usuario u, String senha, String idPerfil) async {
+    SharedPreferences _sharedpreferences =
+        await SharedPreferences.getInstance();
+
+    if (u.id == null) {
+      final Uri uri = Uri.parse('$url/gerente/funcionario/add');
+      final response = await http.post(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${_sharedpreferences.getString('token')}'
+        },
+        body: jsonEncode(<String, String>{
+          'nome': u.nome,
+          'login': u.login,
+          'senha': senha,
+          'cpf': u.cpf,
+          'idPerfil': idPerfil,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Usuario inserido com sucesso: ${response.body}');
+      } else {
+        throw Exception('Falha ao inserir Usuario');
+      }
+    } else {
+      Uri uri = Uri.parse('$url/gerente/funcionario/update/nome');
+      final response = await http.patch(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${_sharedpreferences.getString('token')}'
+        },
+        body: jsonEncode(<String, String>{
+          'id': u.id.toString(),
+          'nome': u.nome,
+        }),
+      );
+      uri = Uri.parse('$url/gerente/funcionario/update/senha');
+      http.patch(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${_sharedpreferences.getString('token')}'
+        },
+        body: jsonEncode(<String, String>{
+          'idUsuario': u.id.toString(),
+          'senha': senha,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Usuario alterado com sucesso: ${response.body}');
+      } else {
+        throw Exception('Falha ao alterar Usuario');
+      }
     }
   }
 }
